3c8d5e549dd42bb64455838878bc607acefeb0ab
Stanimir Ivanov <stanimr@protonmail.com>
2026-08-10 10:46:22 +0200

    [#1] Bootstrap project

---
+++ retrieval-service/src/main/kotlin/org/raghc/retrieval/RetrievalServiceApp.kt
@@ -0,0 +1,11 @@
+package org.raghc.retrieval
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IngestionServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<IngestionServiceApplication>(*args)
+}
